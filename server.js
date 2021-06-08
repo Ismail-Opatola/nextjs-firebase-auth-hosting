e86546b7b36c540442d5d2cbbcefda760c78bc82
Ismail Opatola <opatolamails@gmail.com>
2021-06-08 14:28:21 +0100

    minor updates

--- server.js
+++ server.js
@@ -34,7 +34,5 @@ exports.nextjs = {server};
  * Other Cloud functions
  * ---------------------
  */
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+
